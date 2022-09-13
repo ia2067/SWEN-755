c1b2780273e753fde463ed2240ba4732007264ec
dn8428 <dn8428@rit.edu>
2022-09-12 18:57:34 -0700

    Sensor has a heartbeatSender, ensure we can get to a failure point to kill the process.

--- Assignment1/include/Sensor.hpp
+++ Assignment1/include/Sensor.hpp
@@ -1,9 +1,19 @@
-#ifndef SENSOR_H // Sensor.hpp includes guard.
-#define SENSOR_H
+#ifndef SENSOR_HPP // Sensor.hpp includes guard.
+#define SENSOR_HPP
 
+#include <iostream>
 #include <common/HeartbeatSender.hpp>
 
-class Sensor : public Common::HeartbeatSender{
+class Sensor : public Common::Thread{
+
+public:
+    enum State_e {
+        INIT,
+        MEASURE,
+        FAILURE,
+        DEAD
+    };
+
 public: 
     // Number of samples taken (and averaged) to get a measurement.
     int sample_size;
@@ -16,8 +26,9 @@ public:
      * 
      * @param: sample_size (int): Number of samples to average to get measurement.
      * @param: scaling_factor (double): Factor to use when scaling sensor measurements.
+     * @param: id (string): String ID for our heartbeat sender.
      */
-    Sensor(int sample_size, double scaling_factor);
+    Sensor(int sample_size, double scaling_factor, std::string id);
 
     /*
      * @brief: Take a single random sample (measurement) of the sensor.
@@ -35,10 +46,40 @@ public:
      * @param: center (int): The value samples should be centered around.
      * @param: offset (int): The max distance away from center for measurement.
      *
-     * @return (double) scaled average measurement from all sensor samples.
+     * @return (float) scaled average measurement from all sensor samples.
      */
-    double measure(int center, int offset);
+    float measure(int center, int offset);
+
+
+
+    /**
+     * 
+     */
+    std::string GetID() { return id; }
+
+private:
+    // common::thread
+    void _run() override;
+    State_e _getState();
+    void _setState(State_e);
+
+private:
+    boost::chrono::milliseconds _init();
+    boost::chrono::milliseconds _measure();
+    boost::chrono::milliseconds _failure();
+    
+private:
+    std::shared_ptr<Common::HeartbeatSender> _pHeartbeatSender;
+
+    std::string id;
+
+    State_e _state;
+
+    int sampleVal;
+    float measureVal;
+
+    int numRuns;
 };
 
 
-#endif // !SENSOR_H
+#endif // !SENSOR_HPP
--- Assignment1/src/Sensor.cpp
+++ Assignment1/src/Sensor.cpp
@@ -1,14 +1,24 @@
 #include <stdlib.h>   //rand
 #include <Sensor.hpp>
+#include <time.h>
 
 namespace bc = boost::chrono;
 
 
-Sensor::Sensor(int sample_size, double scaling_factor) 
-: Common::HeartbeatSender("SensorProcess", "myQueue", bc::milliseconds(3000))
+Sensor::Sensor(int sample_size, double scaling_factor, std::string id)
+: _pHeartbeatSender(nullptr),
+  _state(INIT),
+  sampleVal(0),
+  measureVal(0),
+  numRuns(0)
 {
+    srand(time(NULL));
+    
     Sensor::sample_size = sample_size;
     Sensor::scaling_factor = scaling_factor;
+    Sensor::id = id;
+
+    _pHeartbeatSender = std::make_shared<Common::HeartbeatSender>(id, "hbSendQueue");
 }
 
 
@@ -23,9 +33,9 @@ int Sensor::sample(int center, int offset) {
 }
 
 
-double Sensor::measure(int center, int offset) {
+float Sensor::measure(int center, int offset) {
     int rolling_sum = 0;
-    double avg_sample = 0;
+    float avg_sample = 0;
 
     // Take number of samples with rolling sum to calc average.
     for (int i = 0; i < Sensor::sample_size; i++) {
@@ -33,5 +43,70 @@ double Sensor::measure(int center, int offset) {
     }
 
     // Get Average based on rolling sum.
-    return rolling_sum / (double) Sensor::sample_size * Sensor::scaling_factor;
+    return rolling_sum / (float) Sensor::sample_size * Sensor::scaling_factor;
+}
+
+Sensor::State_e Sensor::_getState()
+{
+    return _state;
+}
+
+void Sensor::_setState(State_e state)
+{
+    _state = state;
+}
+
+void Sensor::_run() {
+    do
+    {
+        bc::milliseconds nextSleepTime;
+        switch(_getState())
+        {
+        case INIT:
+            nextSleepTime = _init();
+            break;
+        case MEASURE:
+            nextSleepTime = _measure();
+            break;
+        case FAILURE:
+            nextSleepTime = _failure();
+            break;
+        default:
+            break;
+        }
+        boost::this_thread::sleep_for(nextSleepTime);
+    } while (!_getShutdown());
+
+    _setState(DEAD);
+}
+
+bc::milliseconds Sensor::_init()
+{
+    _pHeartbeatSender->start();
+    _setState(MEASURE);
+    return bc::milliseconds(0);
+}
+
+bc::milliseconds Sensor::_measure()
+{
+    sampleVal = sample(3, 10);
+    measureVal = measure(3, 10);
+
+    std::cout << "Sample Value: " << sampleVal << " Measure Value: " << measureVal << std::endl;
+    numRuns++;
+
+    if(sampleVal == measureVal)
+    {
+        _setState(FAILURE);
+        return bc::milliseconds(0);
+    }
+    return bc::milliseconds(0);
+}
+
+bc::milliseconds Sensor::_failure()
+{
+    std::cout << "Num Runs: " << numRuns << std::endl;
+    _pHeartbeatSender->end();
+    _setState(DEAD);
+    return bc::milliseconds(0);
 }
