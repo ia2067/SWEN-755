37624977fda394e6f4364125bf269add97db182b
ia2067 <ia2067@rit.edu>
2022-10-12 02:08:35 +0000

    Created 'FaultMonitor' in Assignment2 which just wraps up everything that was happening into a single class

---
+++ Assignment2/src/FaultMonitor.cpp
@@ -0,0 +1,207 @@
+#include <FaultMonitor.hpp>
+
+namespace Assignment2
+{
+
+FaultMonitor::FaultMonitor(std::string heartbeatRcvrMQN, std::string primaryMQN, std::string secondaryMQN)
+: _primaryDead(false),
+  _secondaryDead(false),
+  _state(INIT),
+  _heartbeatRcvrMQN(heartbeatRcvrMQN),
+  _primaryMQN(primaryMQN),
+  _secondaryMQN(secondaryMQN),
+  _pHBR(nullptr),
+  _pPrimary(nullptr),
+  _pSecondary(nullptr)
+{ }
+
+
+void FaultMonitor::_run()
+{
+    do
+    {
+        std::chrono::milliseconds nextSleepTime;
+        switch(getState())
+        {
+        case INIT:
+            nextSleepTime = _init();
+            break;
+        case WAKING_PRIMARY:
+            nextSleepTime = _wakingPrimary();
+            break;
+        case RUNNING_PRIMARY:
+            nextSleepTime = _runningPrimary();
+            break;
+        case WAKING_SECONDARY:
+            nextSleepTime = _wakingSecondary();
+            break;
+        case RUNNING_SECONDARY:
+            nextSleepTime = _runningSecondary();
+            break;
+        case ALL_DEAD:
+            nextSleepTime = _allDead();
+            break;
+        default:
+            break;
+        }
+        std::this_thread::sleep_for(nextSleepTime);
+    } while (!_getShutdown());
+
+    _setState(SHUTDOWN);
+}
+
+std::chrono::milliseconds FaultMonitor::_init()
+{
+    _pHBR = std::make_shared<Heartbeat::Receiver>(_heartbeatRcvrMQN,
+                                                  std::chrono::milliseconds(100),
+                                                  std::chrono::milliseconds(3000));
+
+    _pPrimary = std::make_shared<FaultHandle::Client>(_primaryMQN);
+    _pSecondary = std::make_shared<FaultHandle::Client>(_secondaryMQN);
+
+    _pHBR->start();
+    _pHBR->sigNewDead.connect(boost::bind(&FaultMonitor::_handleNewDead, 
+                                          this, 
+                                          boost::placeholders::_1));
+
+    _setState(WAKING_PRIMARY);
+    return std::chrono::milliseconds(1000);
+}
+std::chrono::milliseconds FaultMonitor::_wakingPrimary() 
+{
+    FaultHandle::Message wakeup_primary(FaultHandle::MessageType_e::CMD_WAKEUP,{});
+    if(_pPrimary->query(wakeup_primary))
+    {
+        std::cout << "PRIMARY IS AWAKE!" << std::endl;
+        _setState(RUNNING_PRIMARY);
+    }
+    else
+        std::cout << "PRIMARY DIDNT WAKE UP AWAKE!" << std::endl;
+    return std::chrono::milliseconds(200);
+}
+std::chrono::milliseconds FaultMonitor::_runningPrimary() 
+{
+    if(_getPrimaryDead())
+    {
+        _setState(WAKING_SECONDARY);
+        return std::chrono::milliseconds(100);
+    }
+
+    FaultHandle::Message get_data_cmd(FaultHandle::MessageType_e::CMD_GETDATA,{});
+    if(_pPrimary->query(get_data_cmd))
+    {
+        std::cout << "Got Data(primary): ";
+        for(auto it : get_data_cmd.getSensorData())
+        {
+            std::cout << it << ", " << std::flush;
+        }
+        std::cout << std::endl;
+    }
+    else
+        std::cout << "DIDN'T GET DATA (PRIMARY)" << std::endl;
+
+    return std::chrono::milliseconds(1000);
+}
+std::chrono::milliseconds FaultMonitor::_wakingSecondary() 
+{
+    FaultHandle::Message wake_up_secondary(FaultHandle::MessageType_e::CMD_WAKEUP,{});
+    if(_pSecondary->query(wake_up_secondary))
+    {
+        std::cout << "SECONDARY IS AWAKE!" << std::endl;
+        _setState(RUNNING_SECONDARY);
+    }
+    else
+        std::cout << "SECONDARY DIDNT WAKE UP AWAKE!" << std::endl;
+
+    return std::chrono::milliseconds(200);
+}
+std::chrono::milliseconds FaultMonitor::_runningSecondary() 
+{
+    if(_getSecondaryDead())
+    {
+        _setState(ALL_DEAD);
+        return std::chrono::milliseconds(100);
+    }
+
+    FaultHandle::Message get_data_cmd(FaultHandle::MessageType_e::CMD_GETDATA,{});
+    if(_pSecondary->query(get_data_cmd))
+    {
+        std::cout << "Got Data(secondary): ";
+        for(auto it : get_data_cmd.getSensorData())
+        {
+            std::cout << it << ", " << std::flush;
+        }
+        std::cout << std::endl;
+    }
+    else
+        std::cout << "DIDN'T GET DATA (SECONDARY)" << std::endl;
+
+    return std::chrono::milliseconds(1000);
+}
+std::chrono::milliseconds FaultMonitor::_allDead()
+{
+    std::chrono::system_clock::time_point current_time = std::chrono::system_clock::now();
+    std::time_t tt = std::chrono::system_clock::to_time_t(current_time);
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "current time: " << std::ctime(&tt);
+    std::cout << "------------------------------------" << std::endl;
+    for(auto dead : _pHBR->deadIds())
+    {
+        std::cout << "        DEAD: " << dead << std::endl;
+        std::chrono::system_clock::time_point tp(_pHBR->getLastBeat(dead));
+        std::time_t tt = std::chrono::system_clock::to_time_t(tp);
+        std::cout << "  last comms: " << std::ctime(&tt);
+        std::cout << "------------------------------------" << std::endl;
+    }
+
+    _setState(SHUTDOWN);
+    return std::chrono::milliseconds(1000);
+}
+
+void FaultMonitor::_handleNewDead(std::string id)
+{
+    if (id == "primarySensor")
+    {
+        if(!_getPrimaryDead())
+            std::cout << "PRIMARY IS DEAD!" << std::endl;
+        _cachePrimaryDead(true);
+        
+    }
+    if(id == "secondarySensor")
+    {
+        if(!_getSecondaryDead())
+            std::cout << "SECONDARY IS DEAD!" << std::endl;
+        _cacheSecondaryDead(true);
+    }
+}
+bool FaultMonitor::_getPrimaryDead()
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    return _primaryDead;
+}
+bool FaultMonitor::_getSecondaryDead()
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    return _secondaryDead;
+}
+void FaultMonitor::_cachePrimaryDead(bool isDead)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    _primaryDead = isDead;
+}
+void FaultMonitor::_cacheSecondaryDead(bool isDead)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    _secondaryDead = isDead;
+}
+FaultMonitor::State_e FaultMonitor::getState()
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    return _state;
+}
+void FaultMonitor::_setState(State_e state)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    _state = state;
+}
+} // namespace Assignment2
--- Assignment2/src/main.cpp
+++ Assignment2/src/main.cpp
@@ -1,13 +1,12 @@
 
 
 #include <iostream>
-#include <ctime>
-
-#include <heartbeat/Receiver.hpp>
-#include <faulthandle/Client.hpp>
+#include <csignal>
 
 #include <boost/process.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
+
+#include <FaultMonitor.hpp>
 
 
 int main(int argc, char const *argv[])
@@ -30,122 +29,19 @@ int main(int argc, char const *argv[])
     std::this_thread::sleep_for(std::chrono::seconds(1));
     bp::child secondarySensorProcess("./syncedSensorProcess --size 10 --scaleFactor 2.0 --hbMq myQueue --fhMq secondarySensorQ --id secondarySensor --syncTx null --syncRx syncQueue");
 
-    Heartbeat::Receiver hr(mqn, std::chrono::milliseconds(100), std::chrono::milliseconds(3000));
-    FaultHandle::Client primarySensorChecker(primarySensorQueueName);
-    FaultHandle::Client secondarySensorChecker(secondarySensorQueueName);
+    Assignment2::FaultMonitor fm(mqn, primarySensorQueueName, secondarySensorQueueName);
 
+    std::cout << "Starting Fault Monitor" << std::endl;
+    fm.start();
 
-    hr.start();
-    // hr.addSenderId("primarySensor");
-    // hr.addSenderId("secondarySensor");
-
-    bool primaryDied = false;
-    bool secondaryDied = false;
-    auto fHandlePrimaryDied = [&] (std::string id){
-        if (id == "primarySensor")
-        {
-            if(!primaryDied)
-                std::cout << "PRIMARY IS DEAD!" << std::endl;
-            primaryDied = true;
-            
-        }
-        if(id == "secondarySensor")
-        {
-            if(!secondaryDied)
-                std::cout << "SECONDARY IS DEAD!" << std::endl;
-            secondaryDied = true;
-        }
-
-    };
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    bool primaryAwake = false;
-    while(!primaryAwake)
+    while(fm.getState() != Assignment2::FaultMonitor::State_e::SHUTDOWN)
     {
-        FaultHandle::Message wakeup_primary(FaultHandle::MessageType_e::CMD_WAKEUP,{});
-        if(primarySensorChecker.query(wakeup_primary))
-        {
-            std::cout << "PRIMARY IS AWAKE!" << std::endl;
-            primaryAwake = true;
-        }
-            
-        else
-            std::cout << "PRIMARY DIDNT WAKE UP AWAKE!" << std::endl;
-        
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    }
-    
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    hr.sigNewDead.connect(fHandlePrimaryDied);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-
-    while(!primaryDied)
-    {
-        FaultHandle::Message get_data_cmd(FaultHandle::MessageType_e::CMD_GETDATA,{});
-        if(primarySensorChecker.query(get_data_cmd))
-        {
-            std::cout << "Got Data(primary): ";
-            for(auto it : get_data_cmd.getSensorData())
-            {
-                std::cout << it << ", " << std::flush;
-            }
-            std::cout << std::endl;
-        }
-        else
-            std::cout << "DIDN'T GET DATA (PRIMARY)" << std::endl;
-
-        std::this_thread::sleep_for(std::chrono::seconds(1));   
-    }
-
-
-    bool secondaryAwake = false;
-    while(!secondaryAwake)
-    {
-        FaultHandle::Message wake_up_secondary(FaultHandle::MessageType_e::CMD_WAKEUP,{});
-        if(secondarySensorChecker.query(wake_up_secondary))
-        {
-            std::cout << "SECONDARY IS AWAKE!" << std::endl;
-            secondaryAwake = true;
-        }
-        else
-            std::cout << "SECONDARY DIDNT WAKE UP AWAKE!" << std::endl;
-    }
-    
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-
-    while(!secondaryDied)
-    {
-        FaultHandle::Message get_data_cmd(FaultHandle::MessageType_e::CMD_GETDATA,{});
-        if(secondarySensorChecker.query(get_data_cmd))
-        {
-            std::cout << "Got Data(secondary): ";
-            for(auto it : get_data_cmd.getSensorData())
-            {
-                std::cout << it << ", " << std::flush;
-            }
-            std::cout << std::endl;
-        }
-        else
-            std::cout << "DIDN'T GET DATA (SECONDARY)" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-    
 
-    std::chrono::system_clock::time_point current_time = std::chrono::system_clock::now();
-    std::time_t tt = std::chrono::system_clock::to_time_t(current_time);
-    std::cout << "------------------------------------" << std::endl;
-    std::cout << "current time: " << std::ctime(&tt);
-    std::cout << "------------------------------------" << std::endl;
-    for(auto dead : hr.deadIds())
-    {
-        std::cout << "        DEAD: " << dead << std::endl;
-        std::chrono::system_clock::time_point tp(hr.getLastBeat(dead));
-        std::time_t tt = std::chrono::system_clock::to_time_t(tp);
-        std::cout << "  last comms: " << std::ctime(&tt);
-        std::cout << "------------------------------------" << std::endl;
-    }
+    std::cout << "Ending Fault Monitor" << std::endl;
+    fm.end();
 
-    hr.end();
     primarySensorProcess.terminate();
     secondarySensorProcess.terminate();
 
