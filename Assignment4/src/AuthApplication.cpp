d3b31e102ec78efd5e1a73217cdffbfdbf9b48dd
ia2067 <ia2067@rit.edu>
2022-11-09 02:16:02 +0000

    added something to the logged in page

--- Assignment4/src/AuthApplication.cpp
+++ Assignment4/src/AuthApplication.cpp
@@ -1,5 +1,10 @@
 #include <AuthApplication.hpp>
 
+#include <Wt/WText.h>
+#include <Wt/WLineEdit.h>
+#include <Wt/WPushButton.h>
+#include <Wt/WPanel.h>
+
 AuthApplication::AuthApplication(const Wt::WEnvironment& env)
 : Wt::WApplication(env),
   session_(appRoot() + "auth.db")
@@ -23,6 +28,8 @@ AuthApplication::AuthApplication(const Wt::WEnvironment& env)
 
     root()->addWidget(std::move(authWidget));
 
+    _pInitialPanel = CreateInitialPanel();
+
 }
 
 void AuthApplication::authEvent()
@@ -33,6 +40,36 @@ void AuthApplication::authEvent()
         << "User " << u.id()
         << " (" << u.identity(Wt::Auth::Identity::LoginName) << ")"
         << " logged in.";
+
+        root()->addWidget(std::move(_pInitialPanel));
     } else
+    {
       log("notice") << "User logged out.";
+      root()->removeWidget(_pInitialPanel.get());
+    }
+
+}
+
+std::unique_ptr<Wt::WPanel> AuthApplication::CreateInitialPanel()
+{
+    auto panelContainer = std::make_unique<Wt::WContainerWidget>();
+    panelContainer->addWidget(std::make_unique<Wt::WText>("Your name, please? "));
+
+    _pNameEdit =  panelContainer->addWidget(std::make_unique<Wt::WLineEdit>());
+
+    Wt::WPushButton *button =  panelContainer->addWidget(std::make_unique<Wt::WPushButton>("Greet me."));
+    panelContainer->addWidget(std::make_unique<Wt::WBreak>());
+    _pGreeting =  panelContainer->addWidget(std::make_unique<Wt::WText>());
+
+    auto greet = [this]{
+        _pGreeting->setText("Hello There, " + _pNameEdit->text());
+    };
+    button->clicked().connect(greet);
+
+    auto pPanel = std::make_unique<Wt::WPanel>();
+    pPanel->setTitle("My Panel");
+    pPanel->setCollapsible(true);
+    pPanel->setCentralWidget(std::move(panelContainer));
+
+    return pPanel;
 }
